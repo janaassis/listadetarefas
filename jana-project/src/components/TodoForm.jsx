1db62e9e5596a9f524c2b45a6d4038634d1036ce
janaina assis <janaina@Robsons-MacBook-Pro.local>
2024-01-04 15:14:46 -0300

    tarefas

---
+++ jana-project/src/components/TodoForm.jsx
@@ -0,0 +1,40 @@
+import { createFactory, useState } from "react";
+
+const TodoForm = ({ addTodo }) => {
+  const [value, setValue] = useState("");
+  const [category, setCategory] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value || !category) return;
+    console.log(value, category);
+    // adicionar todo
+    addTodo(value, category);
+    setValue("");
+    setCategory("");
+    //limpar os campos
+  };
+
+  return (
+    <div className="todo-form">
+      <h2>Criar tarefa:</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Digite o titulo"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <select value={category} onChange={(e) => setCategory(e.target.value)}>
+          <option value="">Selecione uma categoria</option>
+          <option value="Trabalho">Trabalho</option>
+          <option value="Pessoal">Pessoal</option>
+          <option value="Estudos">Estudos</option>
+        </select>
+        <button type="submit">Criar tarefa</button>
+      </form>
+    </div>
+  );
+};
+
+export default TodoForm;
